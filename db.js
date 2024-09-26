f3a1d77d73e2baa12a6b86b808f4baf72490817a
Merk <ofmencall@gmail.con>
2024-09-26 17:35:59 +0300

    update: sequlize params in to dotenv

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const config = require('./config/config.json').development;
+require('dotenv').config();
 
-const sequelize = new Sequelize('userpanel', 'postgres', 'agugih48', {
-  host: '172.17.0.2',
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
 });
+
 const User = sequelize.define('User', {
   firstName: DataTypes.STRING,
   lastName: DataTypes.STRING,
@@ -20,9 +21,7 @@ const Authentication = sequelize.define('Authentication', {
   password: DataTypes.STRING
 });
 
-
 User.hasOne(Authentication);
 Authentication.belongsTo(User);
-
 
 module.exports = sequelize;
