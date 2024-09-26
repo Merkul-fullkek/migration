c5dbdb25d46db3b795d32ac7370f5f727b00b71e
Merk <ofmencall@gmail.con>
2024-09-26 17:31:19 +0300

    updated

--- db.js
+++ db.js
@@ -1,18 +1,28 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const config = require('./config/config.json').development;
 
-const sequelize = new Sequelize('userpanel', 'daniil', 'agugih48', {
+const sequelize = new Sequelize('userpanel', 'postgres', 'agugih48', {
   host: '172.17.0.2',
   dialect: 'postgres'
 });
+const User = sequelize.define('User', {
+  firstName: DataTypes.STRING,
+  lastName: DataTypes.STRING,
+  middleName: DataTypes.STRING,
+  status: DataTypes.STRING
+});
 
-// Проверка соединения
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+const Authentication = sequelize.define('Authentication', {
+  login: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  password: DataTypes.STRING
+});
+
+
+User.hasOne(Authentication);
+Authentication.belongsTo(User);
+
 
 module.exports = sequelize;
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const basicAuth = require('express-basic-auth');
+const { Authenticate } = require('../models');
+const bcrypt = require('bcrypt');
+
+// Функция для получения пользователей из базы данных
+const getUsers = async () => {
+  const authentications = await Authenticate.findAll({ attributes: ['login', 'password'] });
+  return authentications.reduce((acc, auth) => {
+    acc[auth.login] = auth.password;
+    return acc;
+  }, {});
+};
+
+// Настройки Basic Auth
+const auth = basicAuth({
+  authorizer: async (username, password, cb) => {
+    const users = await getUsers();
+    if (users[username]) {
+      const passwordMatch = await bcrypt.compare(password, users[username]);
+      if (passwordMatch) {
+        return cb(null, true);
+      }
+    }
+    return cb(null, false);
+  },
+  authorizeAsync: true,
+  unauthorizedResponse: (req) => {
+    return { error: 'Unauthorized' };
+  }
+});
+
+module.exports = auth;
