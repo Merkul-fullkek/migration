2ba3457ed2416306acc82f06f7689cdb59a58124
Merk <ofmencall@gmail.con>
2024-10-07 12:02:27 +0300

    upd: add services, rework controllers

--- controllers/user-create.js
+++ controllers/user-create.js
@@ -1,56 +1,10 @@
-const { User, Authenticate } = require('../models');
-const auth = require('../middleware/authMiddleware');
-const bcrypt = require('bcrypt');
-
-exports.createUser = [
-  auth, // Добавляем middleware для Basic Auth
-  async (req, res) => {
-    try {
-      const { firstName, lastName, middleName, status, login, password } = req.body;
-
-      // Проверяем, что все необходимые поля присутствуют
-      if (!firstName || !lastName || !middleName || !status || !login || !password) {
-        return res.status(400).json({ error: 'All fields are required' });
-      }
-
-      // Проверяем, что модели определены
-      if (!User || !Authenticate) {
-        return res.status(500).json({ error: 'Models are not defined' });
-      }
-
-      // Проверяем, существует ли уже такой логин
-      const existingAuthenticate = await Authenticate.findOne({ where: { login } });
-      if (existingAuthenticate) {
-        return res.status(400).json({ error: 'Login already exists' });
-      }
-
-      // Хешируем пароль
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Создаем пользователя и ассоциированную запись аутентификации
-      const user = await User.create(
-        {
-          firstName,
-          lastName,
-          middleName,
-          status,
-          Authentications: { // Используем алиас 'Authentications'
-            login,
-            password: hashedPassword
-          }
-        },
-        {
-          include: [{ model: Authenticate, as: 'Authentications' }] // Используем алиас 'Authentications'
-        }
-      );
-
-      return res.status(201).json(user);
-    } catch (error) {
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        return res.status(400).json({ error: 'Unique constraint error: login already exists' });
-      } else {
-        return res.status(400).json({ error: error.message });
-      }
-    }
+const UserService = require('../services/user-service')
+exports.createUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    const user = await UserService.createUser(userData);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-];
+};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const getAllUsersController = require('../controllers/get-users');
 const updateUserController = require('../controllers/update-user');
 const createUserController = require('../controllers/user-create');
-const deleteUserController = require('../controllers/user-delete')
+const deleteUserController = require('../controllers/user-delete');
+const auth = require('../middleware/authMiddleware');
 
-router.post('/users', createUserController.createUser);
-router.get('/users', getAllUsersController.getAllUsers);
-router.put('/users/:userId', updateUserController.updateUser);
-router.delete('/users/:userId', deleteUserController.deleteUser);
+router.post('/users',auth, createUserController.createUser);
+router.get('/users',auth, getAllUsersController.getAllUsers);
+router.put('/users/:userId',auth, updateUserController.updateUser);
+router.delete('/users/:userId',auth, deleteUserController.deleteUser);
 
 module.exports = router;
