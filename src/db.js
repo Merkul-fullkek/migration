15a85994a590ede635351ae337a0c088209e4bff
Merk <ofmencall@gmail.con>
2024-10-21 13:44:14 +0300

    upd:.gitignore

--- src/db.js
+++ src/db.js
@@ -2,13 +2,13 @@ const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE, // database
-  process.env.DB_USERNAME, // username
-  process.env.DB_PASSWORD, // password
+  process.env.DB_DATABASE, 
+  process.env.DB_USERNAME, 
+  process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT // Добавляем порт, если он указан в .env
+    port: process.env.DB_PORT 
   }
 );
 
